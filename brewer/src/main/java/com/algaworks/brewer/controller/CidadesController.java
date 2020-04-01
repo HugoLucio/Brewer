73286da095fdd4ed9fb070e40a975a4eae081d51
hugolucio <hlucio.edge@scml.pt>
2020-04-01 11:52:58 +0100

    cache para cidade

--- brewer/src/main/java/com/algaworks/brewer/controller/CidadesController.java
+++ brewer/src/main/java/com/algaworks/brewer/controller/CidadesController.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
@@ -27,7 +29,6 @@ import com.algaworks.brewer.repository.filter.CidadeFilter;
 import com.algaworks.brewer.service.CadastroCidadeService;
 import com.algaworks.brewer.service.exception.NomeCidadeJaCadastradaException;
 
-
 @Controller
 @RequestMapping("/cidades")
 public class CidadesController {
@@ -48,16 +49,18 @@ public class CidadesController {
 		return mv;
 	}
 	
+	@Cacheable(value = "cidades", key = "#codigoEstado")
 	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody List<Cidade> pesquisarPorCodigoEstado(
 			@RequestParam(name = "estado", defaultValue = "-1") Long codigoEstado) {
 		try {
-			Thread.sleep(500);
+			Thread.sleep(1000);
 		} catch (InterruptedException e) {	}
 		return cidades.findByEstadoCodigo(codigoEstado);
 	}
 	
 	@PostMapping("/nova")
+	@CacheEvict(value = "cidades", key = "#cidade.estado.codigo", condition = "#cidade.temEstado()")
 	public ModelAndView salvar(@Valid Cidade cidade, BindingResult result, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
 			return nova(cidade);
@@ -85,4 +88,5 @@ public class CidadesController {
 		mv.addObject("pagina", paginaWrapper);
 		return mv;
 	}
+	
 }
