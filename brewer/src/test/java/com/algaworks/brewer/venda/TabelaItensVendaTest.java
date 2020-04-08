7b8c09786e20b328a0de38c6f5561376b7b1fa11
hugolucio <hlucio.edge@scml.pt>
2020-04-08 11:41:57 +0100

    dinamizar os valores na tabela de vendas

--- brewer/src/test/java/com/algaworks/brewer/venda/TabelaItensVendaTest.java
+++ brewer/src/test/java/com/algaworks/brewer/venda/TabelaItensVendaTest.java
@@ -1,6 +1,6 @@
 package com.algaworks.brewer.venda;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.math.BigDecimal;
 
@@ -16,7 +16,7 @@ public class TabelaItensVendaTest {
 	
 	@Before
 	public void setUp() {
-		this.tabelaItensVenda = new TabelaItensVenda();
+		this.tabelaItensVenda = new TabelaItensVenda("1");
 	}
 	
 	@Test
@@ -53,7 +53,6 @@ public class TabelaItensVendaTest {
 		assertEquals(new BigDecimal("18.88"), tabelaItensVenda.getValorTotal());
 	}
 	
-	
 	@Test
 	public void deveManterTamanhoDaListaParaMesmasCervejas() throws Exception {
 		Cerveja c1 = new Cerveja();
@@ -66,4 +65,42 @@ public class TabelaItensVendaTest {
 		assertEquals(1, tabelaItensVenda.total());
 		assertEquals(new BigDecimal("9.00"), tabelaItensVenda.getValorTotal());
 	}
+	
+	@Test
+	public void deveAlterarQuantidadeDoItem() throws Exception {
+		Cerveja c1 = new Cerveja();
+		c1.setCodigo(1L);
+		c1.setValor(new BigDecimal("4.50"));
+		
+		tabelaItensVenda.adicionarItem(c1, 1);
+		tabelaItensVenda.alterarQuantidadeItens(c1, 3);
+		
+		assertEquals(1, tabelaItensVenda.total());
+		assertEquals(new BigDecimal("13.50"), tabelaItensVenda.getValorTotal());
+	}
+	
+	@Test
+	public void deveExcluirItem() throws Exception {
+		Cerveja c1 = new Cerveja();
+		c1.setCodigo(1L);
+		c1.setValor(new BigDecimal("8.90"));
+		
+		Cerveja c2 = new Cerveja();
+		c2.setCodigo(2L);
+		c2.setValor(new BigDecimal("4.99"));
+		
+		Cerveja c3 = new Cerveja();
+		c3.setCodigo(3L);
+		c3.setValor(new BigDecimal("2.00"));
+		
+		tabelaItensVenda.adicionarItem(c1, 1);
+		tabelaItensVenda.adicionarItem(c2, 2);
+		tabelaItensVenda.adicionarItem(c3, 1);
+		
+		tabelaItensVenda.excluirItem(c2);
+		
+		assertEquals(2, tabelaItensVenda.total());
+		assertEquals(new BigDecimal("10.90"), tabelaItensVenda.getValorTotal());
+	}
+	
 }
